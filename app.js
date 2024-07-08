53cd52fd67a05f0b31059aa439840587b318da1d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-08 23:03:40 +0300

    Commit

--- app.js
+++ app.js
@@ -5,46 +5,93 @@
 
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
 
-// Load environment variables
-dotenv.config();
+require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
-// Accept json data:
+// Accept json data and convert to object:
 app.use(express.json());
 
-app.all("/", (req, res) => {
-  res.send("WELCOME TO TODO API");
-});
+// app.all('/', (req, res) => {
+//     res.send('WELCOME TO TODO API')
+// })
 
 /* ------------------------------------------------------- */
 // SEQUELIZE:
+// $ npm i sequelize sqlite3
+
 const { Sequelize, DataTypes } = require("sequelize");
 
-// Connection:
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: process.env.SQLITE || "./db.sqlite3",
-});
+// Connection Object:
+// const sequelize = new Sequelize('sqlite:./db.sqlite3')
+// const sequelize = new Sequelize('sqlite:' + process.env.SQLITE)
+const sequelize = new Sequelize(
+  "sqlite:" + (process.env.SQLITE || "./db.sqlite3")
+);
 
 // Sequelize Model:
+// Her bir model, veritabanında bir tabloya karşılık gelir.
+// sequelize.define('tableName', { ...columns })
 const Todo = sequelize.define("todos", {
-  id: {
-    type: DataTypes.INTEGER,
+  // ID sutunu belirtmeye gerek yoktur. Sequelize ID sutununun otomatik oluşturur:
+  // id: {
+  //     type: DataTypes.INTEGER, // DataType // sutun veri tipi.
+  //     allowNull: false, // default: true // sutun verisi boş olabilir mi?
+  //     unique: true, // default: false // benzersiz kayıt mı?
+  //     defaultValue: 0, // Kayıt eklendiğinde default olarak ne yazılsın?
+  //     // autoIncrement: true, // default: false, // Sutun değeri her bir jayıtta otomatik olarak +1 artsın mı?
+  //     // primaryKey: true, // default: false // tablonun her bir kaydını ifade eden benzersiz numara.
+  //     // comment: 'yorum ekleyebiliriz',
+  //     // field: 'custom_field_name'
+  // },
+
+  title: {
+    type: DataTypes.STRING(256), // varchar(256)
     allowNull: false,
-    unique: true,
-    autoIncrement: true,
-    primaryKey: true,
   },
-  task: {
-    type: DataTypes.STRING,
+
+  description: DataTypes.TEXT, // ShortHand
+
+  priority: {
+    // 1: High, 0: Normal, -1: Low
+    type: DataTypes.TINYINT,
     allowNull: false,
+    defaultValue: 0,
   },
+
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+
+  // createdAt ve updatedAt tanımlamaya gerek yok. Otomatik oluşturulur.
 });
 
+// Syncronization: (Bir kere çalıştır.)
+// Modelleri veritabanına uygula:
+//sequelize.sync(); // CREATE TABLE // First Command.
+// sequelize.sync({ force: true }) // DROP TABLE & CREATE TABLE
+// sequelize.sync({ alter: true }) // TO BACKUP & DROP TABLE & CREATE TABLE & FROM BACKUP
+
+// Connect to DB:
+sequelize
+  .authenticate()
+  .then(() => console.log("* DB Connected *"))
+  .catch(() => console.log("* DB Not Connected *"));
+
 /* ------------------------------------------------------- */
+// ROUTES:
+
+const router = express.Router();
+
+// CREATE TODO:
+router.post("/", (req, res) => {
+  const receivedData = req.body;
+  console.log(receivedData);
+});
+
 /* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
@@ -53,21 +100,12 @@ const errorHandler = (err, req, res, next) => {
   const errorStatusCode = res.errorStatusCode ?? 500;
   console.log("errorHandler worked.");
   res.status(errorStatusCode).send({
-    error: true,
-    message: err.message,
-    cause: err.cause,
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
   });
 };
 app.use(errorHandler);
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
-
-// Sync Sequelize models
-sequelize
-  .sync()
-  .then(() => {
-    console.log("Database synced.");
-  })
-  .catch((err) => {
-    console.error("Unable to sync the database:", err);
-  });
